b4d1656f0dc6c3e333513c950e309f58ad90bad1
dlscks <dlscks971026@gmail.com>
2023-02-10 16:38:40 +0900

    up

--- springboot03_board/src/main/java/com/example/security/service/CorsRefFilter.java
+++ springboot03_board/src/main/java/com/example/security/service/CorsRefFilter.java
@@ -35,11 +35,14 @@ public class CorsRefFilter implements Filter{
 	        HttpServletRequest request = (HttpServletRequest) req;
 	        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
 	        response.setHeader("Access-Control-Allow-Credentials", "true");
+	        
 	        response.setHeader("Access-Control-Allow-Methods",
 	                "ACL, CANCELUPLOAD, CHECKIN, CHECKOUT, COPY, DELETE, GET, HEAD, LOCK, MKCALENDAR, MKCOL, MOVE, OPTIONS, POST, PROPFIND, PROPPATCH, PUT, REPORT, SEARCH, UNCHECKOUT, UNLOCK, UPDATE, VERSION-CONTROL");
 	        response.setHeader("Access-Control-Max-Age", "3600");
 	        response.setHeader("Access-Control-Allow-Headers",
 	                "Origin, X-Requested-With, Content-Type, Accept, Key, Authorization");
+	        response.setHeader("Access-Control-Expose-Headers",
+	                "Authorization");
 
 
 	        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
